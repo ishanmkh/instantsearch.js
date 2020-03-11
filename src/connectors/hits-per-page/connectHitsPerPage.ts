ce4d836d2f75fe4db5cb2c4712fd134e88ce8f8c
Nuno Maduro <enunomaduro@gmail.com>
2020-03-11 15:19:04 +0100

    chore(types): migrates hits per page connector to ts

--- src/connectors/hits-per-page/connectHitsPerPage.ts
+++ src/connectors/hits-per-page/connectHitsPerPage.ts
@@ -4,50 +4,97 @@ import {
   createDocumentationMessageGenerator,
   noop,
 } from '../../lib/utils';
+import { WidgetFactory, Renderer, RendererOptions } from '../../types';
+
+import { SearchParameters } from 'algoliasearch-helper';
 
 const withUsage = createDocumentationMessageGenerator({
   name: 'hits-per-page',
   connector: true,
 });
 
-/**
- * @typedef {Object} HitsPerPageRenderingOptionsItem
- * @property {number} value Number of hits to display per page.
- * @property {string} label Label to display in the option.
- * @property {boolean} isRefined Indicates if it's the current refined value.
- */
+export type HitsPerPageRenderingOptionsItem = {
+  /**
+   * Label to display in the option.
+   */
+  label: string;
 
-/**
- * @typedef {Object} HitsPerPageWidgetOptionsItem
- * @property {number} value Number of hits to display per page.
- * @property {string} label Label to display in the option.
- * @property {boolean} default The default hits per page on first search.
- */
+  /**
+   * Number of hits to display per page.
+   */
+  value: number;
 
-/**
- * @typedef {Object} HitsPerPageRenderingOptions
- * @property {HitsPerPageRenderingOptionsItem[]} items Array of objects defining the different values and labels.
- * @property {function(item.value)} createURL Creates the URL for a single item name in the list.
- * @property {function(number)} refine Sets the number of hits per page and trigger a search.
- * @property {boolean} hasNoResults `true` if the last search contains no result.
- * @property {Object} widgetParams Original `HitsPerPageWidgetOptions` forwarded to `renderFn`.
- */
+  /**
+   * Indicates if it's the current refined value.
+   */
+  isRefined: boolean;
+};
 
-/**
- * @typedef {Object} HitsPerPageWidgetOptions
- * @property {HitsPerPageWidgetOptionsItem[]} items Array of objects defining the different values and labels.
- * @property {function(object[]):object[]} [transformItems] Function to transform the items passed to the templates.
- */
+export type HitsPerPageWidgetOptionsItem = {
+  /**
+   * Label to display in the option.
+   */
+  label: string;
+
+  /**
+   * Number of hits to display per page.
+   */
+  value?: number;
+
+  /**
+   * The default hits per page on first search.
+   *
+   * @default false
+   */
+  default?: boolean;
+};
+
+export type HitsPerPageWidgetOptions = {
+  /**
+   * Array of objects defining the different values and labels.
+   */
+  items: HitsPerPageWidgetOptionsItem[];
+
+  /**
+   * Function to transform the items passed to the templates.
+   */
+  transformItems?: (
+    objects: HitsPerPageWidgetOptionsItem[]
+  ) => HitsPerPageWidgetOptionsItem[];
+} & RendererOptions;
+
+export type HitsPerPageRenderingOptions = {
+  /**
+   * Array of objects defining the different values and labels.
+   */
+  items: HitsPerPageRenderingOptionsItem[];
+
+  /**
+   * Creates the URL for a single item name in the list.
+   */
+  createURL: (value: HitsPerPageRenderingOptionsItem['value']) => string;
+
+  /**
+   * Sets the number of hits per page and trigger a search.
+   */
+  refine: (number: number) => any;
+
+  /**
+   * `true` if the last search contains no result.
+   */
+  hasNoResults: boolean;
+
+  /**
+   * Original `HitsPerPageWidgetOptions` forwarded to `renderFn`.
+   */
+  widgetParams: HitsPerPageWidgetOptions;
+};
 
 /**
  * **HitsPerPage** connector provides the logic to create custom widget that will
  * allow a user to choose to display more or less results from Algolia.
  *
  * This connector provides a `refine()` function to change the hits per page configuration and trigger a new search.
- * @type {Connector}
- * @param {function(HitsPerPageRenderingOptions, boolean)} renderFn Rendering function for the custom **HitsPerPage** widget.
- * @param {function} unmountFn Unmount function called when the widget is disposed.
- * @return {function(HitsPerPageWidgetOptions)} Re-usable widget factory for a custom **HitsPerPage** widget.
  * @example
  * // custom `renderFn` to render the custom HitsPerPage widget
  * function renderFn(HitsPerPageRenderingOptions, isFirstRendering) {
@@ -94,10 +141,32 @@ const withUsage = createDocumentationMessageGenerator({
  *   })
  * ]);
  */
-export default function connectHitsPerPage(renderFn, unmountFn = noop) {
+
+export type HitsPerPageRenderer = Renderer<
+  HitsPerPageRenderingOptions & RendererOptions
+>;
+
+export type HitsPerPageWitgetFactory = WidgetFactory<HitsPerPageWidgetOptions>;
+
+type HitsPerPageConnector = (
+  /**
+   * Rendering function for the custom **HitsPerPage** widget.
+   */
+  render: HitsPerPageRenderer,
+
+  /**
+   * Unmount function called when the widget is disposed.
+   */
+  unmount: () => void
+) => HitsPerPageWitgetFactory;
+
+const connector: HitsPerPageConnector = function connectHitsPerPage(
+  renderFn,
+  unmountFn = noop
+) {
   checkRendering(renderFn, withUsage());
 
-  return (widgetParams = {}) => {
+  return (widgetParams = {} as HitsPerPageWidgetOptions) => {
     const { items: userItems, transformItems = items => items } = widgetParams;
     let items = userItems;
 
@@ -123,6 +192,21 @@ export default function connectHitsPerPage(renderFn, unmountFn = noop) {
 
     const defaultItem = defaultItems[0];
 
+    const normalizeItems = ({
+      hitsPerPage,
+    }: SearchParameters): HitsPerPageWidgetOptionsItem[] => {
+      return items.map(item => ({
+        ...item,
+        isRefined: Number(item.value) === Number(hitsPerPage),
+      }));
+    };
+
+    let setHitsPerPage: (value: HitsPerPageWidgetOptionsItem['value']) => void;
+
+    let createURLFactory: (
+      state: SearchParameters
+    ) => HitsPerPageRenderingOptions['createURL'];
+
     return {
       $$type: 'ais.hitsPerPage',
 
@@ -130,6 +214,12 @@ export default function connectHitsPerPage(renderFn, unmountFn = noop) {
         const isCurrentInOptions = items.some(
           item => Number(state.hitsPerPage) === Number(item.value)
         );
+
+        setHitsPerPage = value => {
+          return !value && value !== 0
+            ? helper.setQueryParameter('hitsPerPage', undefined).search()
+            : helper.setQueryParameter('hitsPerPage', value).search();
+        };
 
         if (!isCurrentInOptions) {
           warning(
@@ -150,27 +240,27 @@ The \`items\` option of \`hitsPerPage\` does not contain the "hits per page" val
 You may want to add another entry to the \`items\` option with this value.`
           );
 
-          items = [{ value: '', label: '' }, ...items];
+          items = [
+            // The helper will convert the empty string to `undefined`.
+            { value: ('' as unknown) as undefined, label: '' },
+            ...items,
+          ];
         }
 
-        this.setHitsPerPage = value =>
-          !value && value !== 0
-            ? helper.setQueryParameter('hitsPerPage', undefined).search()
-            : helper.setQueryParameter('hitsPerPage', value).search();
-
-        this.createURL = helperState => value =>
-          createURL(
+        createURLFactory = helperState => value => {
+          return createURL(
             helperState.setQueryParameter(
               'hitsPerPage',
               !value && value !== 0 ? undefined : value
             )
           );
+        };
 
         renderFn(
           {
-            items: transformItems(this._normalizeItems(state)),
-            refine: this.setHitsPerPage,
-            createURL: this.createURL(helper.state),
+            items: transformItems(normalizeItems(state)),
+            refine: setHitsPerPage,
+            createURL: createURLFactory(helper.state),
             hasNoResults: true,
             widgetParams,
             instantSearchInstance,
@@ -184,22 +274,15 @@ You may want to add another entry to the \`items\` option with this value.`
 
         renderFn(
           {
-            items: transformItems(this._normalizeItems(state)),
-            refine: this.setHitsPerPage,
-            createURL: this.createURL(state),
+            items: transformItems(normalizeItems(state)),
+            refine: setHitsPerPage,
+            createURL: createURLFactory(state),
             hasNoResults,
             widgetParams,
             instantSearchInstance,
           },
           false
         );
-      },
-
-      _normalizeItems({ hitsPerPage }) {
-        return items.map(item => ({
-          ...item,
-          isRefined: Number(item.value) === Number(hitsPerPage),
-        }));
       },
 
       dispose({ state }) {
@@ -228,4 +311,6 @@ You may want to add another entry to the \`items\` option with this value.`
       },
     };
   };
-}
+};
+
+export default connector;
